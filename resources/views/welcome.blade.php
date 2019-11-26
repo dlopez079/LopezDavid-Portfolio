ac8d2fc8c45c33d23a3010943db5b10edb06b17b
dlopez079 <dlopez079@gmail.com>
2019-11-26 07:28:11 -0500

    Disabled Gitignore in public assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -116,6 +116,7 @@
 
         <!-- The video -->
         <video autoplay muted loop id="myVideo">
+            <!-- echo asset('storage/img/worldvideo-fullhd.mp4');  -->
             <source src="storage/img/worldvideo-fullhd.mp4" type="video/mp4">
         </video>
 
