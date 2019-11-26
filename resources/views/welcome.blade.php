3c49129e0b6b890ea305aa8a29713328827d7adb
dlopez079 <dlopez079@gmail.com>
2019-11-25 20:57:57 -0500

    Made a change to the mp4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     <style>
         html,
         body {
-            background-image: (url: /) background-color: #fff;
+            background-color: #fff;
             color: #636b6f;
             font-family: 'Nunito', sans-serif;
             font-weight: 200;
