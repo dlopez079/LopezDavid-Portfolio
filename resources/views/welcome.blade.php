9fae4aad0dcce54123e02cdd692e7dba4640d471
dlopez079 <dlopez079@gmail.com>
2019-12-03 13:36:26 -0500

    Final Changes befor push to Dreamhost

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,15 +128,6 @@
             <source src='storage/img/worldvideo-fullhd.mp4' type="video/mp4">
         </video>
         
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-            <img src="storage/img/David_Lopez.png" alt="David Lopez">
-                <h1 class="display-4">David Lopez</h1>
-                <p class="lead">I am revising my webpage.  Doesn't look good right?!? </p>
-                <p class="lead">That's cause I like developing on the fly.  </p>
-                <p class="lead"> Come back at the end of the day and let me know what you think about my site. I will finisht at 5 pm today!!!</p>
-            </div>
-        </div>
 
 
         <div class="content">
